5d1cca687540062662c06976d9c0413e4eb725c8
HUANG SIZHE <hsz1273327@gmail.com>
2021-07-03 23:29:10 +0800

    update

--- test_postgresql_kernel.py
+++ test_postgresql_kernel.py
@@ -3,27 +3,16 @@ import sys
 import unittest
 import jupyter_kernel_test as jkt
 
+helloworld = """ -- connection: postgres://postgres:postgres@localhost:5432/postgres
+-- autocommit: true
+select 'hello, world' as helloworld
+"""
+
 
 class PGKernelTests(jkt.KernelTests):
     kernel_name = "postgresql"
-
     language_name = "sql"
-
-    code_hello_world = ""
-
-    code_display_data = [
-        {'code': '%plot -f png\nplot([1,2,3])', 'mime': 'image/png'},
-        {'code': '%plot -f svg\nplot([1,2,3])', 'mime': 'image/svg+xml'}
-    ] if sys.platform == 'darwin' else []
-
-    completion_samples = [
-        {
-            'text': 'acos',
-            'matches': {'acos', 'acosd', 'acosh'},
-        },
-    ]
-
-    code_page_something = "ones?"
+    code_hello_world = helloworld
 
 
 if __name__ == '__main__':
